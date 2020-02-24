28ae0824f0f48100697cd28f03b2e1b7bda2fc4d
hsytkm <t_hosoya1214@yahoo.co.jp>
2020-02-24 16:36:21 +0900

    Add Controls

---
+++ XFControlSamples/Views/PickerPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFControlSamples.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PickerPage : ContentPage
+    {
+        public PickerPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new PickerViewModel();
+        }
+    }
+
+    class PickerViewModel : INotifyPropertyChanged
+    {
+        public IList<string> ItemsSource => Models.SampleData.Data;
+
+        public string SelectedItem
+        {
+            get => _selectedItem;
+            set => SetProperty(ref _selectedItem, value);
+        }
+        private string _selectedItem = Models.SampleData.Data.First();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual bool SetProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+    }
+}
---
+++ XFControlSamples/Views/ProcessingPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFControlSamples.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProcessingPage : ContentPage
+    {
+        public ProcessingPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new ProcessingViewModel();
+        }
+    }
+
+    class ProcessingViewModel : INotifyPropertyChanged
+    {
+        private const int TimeCounterMax = 6;   // 6秒計測
+
+        public double ProgressRatio
+        {
+            get => _progressRatio;
+            set => SetProperty(ref _progressRatio, value);
+        }
+        private double _progressRatio;
+
+        public bool IsProcessing
+        {
+            get => _isProcessing;
+            set => SetProperty(ref _isProcessing, value);
+        }
+        private bool _isProcessing;
+
+        public ICommand StartProcess => _startProcess ??
+            (_startProcess = new Command(async () =>
+            {
+                IsProcessing = true;
+                ProgressRatio = 0;
+
+                var sec = 0;
+                do
+                {
+                    await Task.Delay(1000);
+                    ProgressRatio = (++sec) / (double)TimeCounterMax;
+                }
+                while (sec < TimeCounterMax);
+
+                IsProcessing = false;
+            }));
+        private ICommand _startProcess;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual bool SetProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+    }
+}
