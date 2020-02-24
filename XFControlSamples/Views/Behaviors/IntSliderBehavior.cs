a3efd6d8a97ee8931f66c725ec939354f7762b4c
hsytkm <t_hosoya1214@yahoo.co.jp>
2020-02-24 18:27:08 +0900

    Add SliderControl

---
+++ XFControlSamples/Views/Behaviors/IntSliderBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XFControlSamples.Views.Behaviors
+{
+    class IntSliderBehavior : Behavior<Slider>
+    {
+        public static readonly BindableProperty IntValueProperty = BindableProperty.Create(
+            nameof(IntValue),
+            typeof(int),
+            typeof(IntSliderBehavior),
+            default(int),
+            BindingMode.OneWayToSource);
+
+        public int IntValue
+        {
+            get => (int)GetValue(IntValueProperty);
+            set => SetValue(IntValueProperty, value);
+        }
+
+        protected override void OnAttachedTo(BindableObject bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            if (!(bindable is Slider slider)) return;
+            slider.ValueChanged += Slider_ValueChanged;
+        }
+
+        protected override void OnDetachingFrom(BindableObject bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            if (!(bindable is Slider slider)) return;
+            slider.ValueChanged -= Slider_ValueChanged;
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            IntValue = (int)e.NewValue;
+        }
+    }
+}
---
+++ XFControlSamples/Views/SliderPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFControlSamples.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SliderPage : ContentPage
+    {
+        public SliderPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new SliderViewModel();
+        }
+    }
+
+    public class SliderViewModel : INotifyPropertyChanged
+    {
+        public double SliderMin => -5;
+        public double SliderMax => 5;
+
+        public double SliderValue
+        {
+            get => _sliderValue;
+            set => SetProperty(ref _sliderValue, value);
+        }
+        private double _sliderValue;
+
+        public int SliderIntValue
+        {
+            get => _sliderIntValue;
+            set => SetProperty(ref _sliderIntValue, value);
+        }
+        private int _sliderIntValue;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual bool SetProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+    }
+}
