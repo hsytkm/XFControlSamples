13040efeb4cc30a373f2cde55416a7d5213be05d
hsytkm <t_hosoya1214@yahoo.co.jp>
2020-02-24 14:59:58 +0900

    Add Controls

---
+++ XFControlSamples/Views/BoxViewPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFControlSamples.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BoxViewPage : ContentPage
+    {
+        public BoxViewPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ContentPage_SizeChanged(object sender, EventArgs e)
+        {
+            // 記事ではBoxRendererでPlatform毎に対応が必要とあったけど、Pixel3では意図通りに表示された
+            var ratio = 0.9;
+
+            var (boxWidth, boxHeight) = (Width * ratio, Height * ratio);
+            boxView.WidthRequest = boxWidth;
+            boxView.HeightRequest = boxHeight;
+
+            var message = new[]
+            {
+                $"PageSize: {Width:f3} x {Height:f3}",
+                $"BoxSize: {boxWidth:f2} x {boxHeight:f2}"
+            };
+            labelSize.Text = string.Join(Environment.NewLine, message);
+        }
+    }
+}
---
+++ XFControlSamples/Views/CheckBoxPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFControlSamples.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CheckBoxPage : ContentPage
+    {
+        public CheckBoxPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new CheckBoxViewModel();
+        }
+    }
+
+    class CheckBoxViewModel : INotifyPropertyChanged
+    {
+        public bool IsEnable {
+            get => _isEnable;
+            set => SetProperty(ref _isEnable, value);
+        }
+        private bool _isEnable;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual bool SetProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+    }
+}
