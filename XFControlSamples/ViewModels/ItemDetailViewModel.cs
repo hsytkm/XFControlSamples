31e5e731e16b7ba630f19cf5ea3f346780a574ad
hsytkm <t_hosoya1214@yahoo.co.jp>
2020-02-24 11:09:09 +0900

    プロジェクト ファイルを追加します。

---
+++ XFControlSamples/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XFControlSamples.Models;
+
+namespace XFControlSamples.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
