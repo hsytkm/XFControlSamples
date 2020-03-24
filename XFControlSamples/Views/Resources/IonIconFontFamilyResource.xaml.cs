7b82cda7271aea7adaa7f8e9c11d295a4725ccc6
hsytkm <t_hosoya1214@yahoo.co.jp>
2020-03-24 22:03:17 +0900

    Add ResourceDictionary.MergedDictionaries

---
+++ XFControlSamples/Views/Resources/IonIconFontFamilyResource.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFControlSamples.Views.Resources
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class IonIconFontFamilyResource : ResourceDictionary
+    {
+        public IonIconFontFamilyResource()
+        {
+            InitializeComponent();
+        }
+    }
+}
