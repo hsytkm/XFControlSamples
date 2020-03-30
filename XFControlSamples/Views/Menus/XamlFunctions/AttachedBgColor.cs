a79339112e90e569e85e293a6a21af1892a9e979
hsytkm <t_hosoya1214@yahoo.co.jp>
2020-03-30 22:29:03 +0900

    Add Attached/Bindable Property

---
+++ XFControlSamples/Views/Menus/XamlFunctions/AttachedBgColor.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace XFControlSamples.Views.Menus
+{
+    class AttachedBgColor
+    {
+        // 添付プロパティ
+        // https://docs.microsoft.com/ja-jp/xamarin/xamarin-forms/xaml/attached-properties
+
+        // ◆あんまちゃんとした例になってない…
+        public static readonly BindableProperty BgColorProperty =
+            BindableProperty.CreateAttached(
+                propertyName: "BgColor",
+                returnType: typeof(Color),
+                declaringType: typeof(AttachedPropertyPage),
+                defaultValue: Color.Transparent,
+                // 以降はデフォルト引数あり省略可
+                defaultBindingMode: BindingMode.OneWay,
+                validateValue: IsValidBgColor,
+                propertyChanged: OnBgColorPropertyChanged,
+                propertyChanging: OnBgColorPropertyChanging,
+                coerceValue: CoerceBgColor,
+                defaultValueCreator: DefaultBgColorCreator);
+
+        public static Color GetBgColor(BindableObject bindable) =>
+            (Color)bindable.GetValue(BgColorProperty);
+
+        public static void SetBgColor(BindableObject bindable, Color value) =>
+            bindable.SetValue(BgColorProperty, value);
+
+        // 検証コールバックによって false が返されると、例外が発生します。
+        private static bool IsValidBgColor(BindableObject bindable, object value)
+        {
+            var isValid = (value is Color);
+
+            Debug.WriteLine($"{nameof(AttachedBgColor)}_validateValue: {isValid}");
+            return isValid;
+        }
+
+        private static void OnBgColorPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is VisualElement visual && newValue is Color newColor)
+            {
+                visual.BackgroundColor = newColor;
+            }
+            Debug.WriteLine($"{nameof(AttachedBgColor)}_propertyChanged: {oldValue} -> {newValue}");
+        }
+
+        // ◆Changed よりも先に呼ばれるが、どのように使い分けるのか謎…
+        private static void OnBgColorPropertyChanging(BindableObject bindable, object oldValue, object newValue)
+        {
+            Debug.WriteLine($"{nameof(AttachedBgColor)}_propertyChanging: {oldValue} -> {newValue}");
+        }
+
+        // 値を補正できる ※Coerce=(人に)強制して(力ずくで)～させる
+        private static object CoerceBgColor(BindableObject bindable, object value)
+        {
+            if (value is Color color)
+            {
+                if (color == Color.Transparent) color = Color.Red;
+
+                Debug.WriteLine($"{nameof(AttachedBgColor)}_coerceValue: {color}");
+                return color;
+            }
+            return value;
+        }
+
+        // デフォルト値を生成できる
+        private static object DefaultBgColorCreator(BindableObject bindable)
+        {
+            var list = Models.SampleData.XamarinFormsColors;
+            var index = new Random().Next(0, list.Count());  // maxValueは含まれない
+            var color = list[index].Color;
+
+            Debug.WriteLine($"{nameof(AttachedBgColor)}_defaultValueCreator: {color}");
+            return color;
+        }
+
+    }
+}
---
+++ XFControlSamples/Views/Menus/XamlFunctions/BindablePropertyPage.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace XFControlSamples.Views.Menus
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BindablePropertyPage : ContentPage
+    {
+        private const int MinAge = 0;
+        private const int MaxAge = 128;
+
+        // Xamarin.Formsのバインド可能なプロパティ
+        // https://docs.microsoft.com/ja-jp/xamarin/xamarin-forms/xaml/bindable-properties
+
+        public static readonly BindableProperty AgeProperty =
+            BindableProperty.Create(
+                propertyName: nameof(Age),
+                returnType: typeof(string),
+                declaringType: typeof(BindablePropertyPage),
+                // 以降はデフォルト引数あり省略可
+                defaultValue: default(string),
+                defaultBindingMode: BindingMode.OneWay,
+                validateValue: IsValidAge,
+                propertyChanged: OnAgePropertyChanged,
+                propertyChanging: OnAgePropertyChanging,
+                coerceValue: CoerceAge,
+                defaultValueCreator: DefaultAgeCreator);
+
+        public string Age
+        {
+            get => (string)GetValue(AgeProperty);
+            set
+            {
+                try { SetValue(AgeProperty, value); }
+                catch (Exception ex) { this.AppendLog(ex.GetType().ToString()); }
+            }
+        }
+
+        // 検証コールバックによって false が返されると、例外が発生します。
+        private static bool IsValidAge(BindableObject bindable, object value)
+        {
+            var isValid = false;
+
+            // 空ならログをクリア
+            if ((value is string s) && string.IsNullOrEmpty(s))
+            {
+                ((BindablePropertyPage)bindable).ClearLog();
+                return isValid;
+            }
+
+            // 年齢が負数はあり得ない
+            if (int.TryParse(value.ToString(), out var age))
+                isValid = (0 <= age);
+
+            ((BindablePropertyPage)bindable).AppendLog($"validateValue: {isValid}");
+            return isValid;
+        }
+
+        private static void OnAgePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (!int.TryParse(oldValue.ToString(), out var oldAge)) return;
+            if (!int.TryParse(newValue.ToString(), out var newAge)) return;
+
+            ((BindablePropertyPage)bindable).AppendLog($"propertyChanged: {oldAge} -> {newAge}");
+        }
+
+        // ◆Changed よりも先に呼ばれるが、どのように使い分けるのか謎…
+        private static void OnAgePropertyChanging(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (!int.TryParse(oldValue.ToString(), out var oldAge)) return;
+            if (!int.TryParse(newValue.ToString(), out var newAge)) return;
+
+            ((BindablePropertyPage)bindable).AppendLog($"propertyChanging: {oldAge} -> {newAge}");
+        }
+
+        // 値を補正できる ※Coerce=(人に)強制して(力ずくで)～させる
+        private static object CoerceAge(BindableObject bindable, object value)
+        {
+            if (int.TryParse(value.ToString(), out var oldAge))
+            {
+                var newAge = oldAge.Clamp(MinAge, MaxAge);
+
+                ((BindablePropertyPage)bindable).AppendLog($"coerceValue: {oldAge} -> {newAge}");
+                return newAge.ToString();
+            }
+            return value;
+        }
+
+        // デフォルト値を生成できる
+        private static string DefaultAgeCreator(BindableObject bindable)
+        {
+            var age = new Random().Next(MinAge, 1000);  // maxValueは含まれない
+
+            ((BindablePropertyPage)bindable).AppendLog($"defaultValueCreator: {age}");
+            return age.ToString();
+        }
+
+        private readonly StringBuilder _stringBuilder = new StringBuilder();
+
+        public BindablePropertyPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AppendLog(string message)
+        {
+            _stringBuilder.AppendLine(message);
+            BindingContext = _stringBuilder.ToString();
+        }
+
+        private void ClearLog()
+        {
+            _stringBuilder.Clear();
+            BindingContext = _stringBuilder.ToString();
+        }
+
+    }
+}
