0c69b8f6b756dbe4743f10e24ac2ebfe9fa306b1
hsytkm <t_hosoya1214@yahoo.co.jp>
2020-04-06 23:44:40 +0900

    Add LineBreakSpan

---
+++ XFControlSamples/Views/Controls/LineBreakSpan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XFControlSamples.Views.Controls
+{
+    class LineBreakSpan : Span
+    {
+        public LineBreakSpan()
+        {
+            Text = Environment.NewLine;
+        }
+    }
+}
